89b7c6d8803c9d7b11b3002d4e6e37605735b9e7
YeonShin <yeonnim82@gmail.com>
2024-12-02 23:07:39 +0900

    코드 컴포넌트화

---
+++ config/db_connection.py
@@ -0,0 +1,18 @@
+import mysql.connector
+from mysql.connector import Error
+
+# 데이터베이스 Network 연결 함수
+def connect_db():
+  try:
+    mydb = mysql.connector.connect(
+      host="192.168.56.101",  
+      port=4567,
+      user="yeonshinkim",
+      passwd="1234",
+    )
+    if mydb.is_connected():
+      print("MySQL 데이터베이스에 성공적으로 연결되었습니다!")
+    return mydb
+  except Error as e:
+    print(f"데이터베이스 연결에 실패했습니다: {e}")
+    return None
--- main.py
+++ main.py
@@ -1,55 +1,7 @@
-import mysql.connector
-from mysql.connector import Error
+from config.db_connection import connect_db
+from config.db_init import create_database_if_not_exists, use_database, init_database
 import os
 
-def connect_db():
-  try:
-    mydb = mysql.connector.connect(
-      host="192.168.56.101",  
-      port=4567,
-      user="yeonshinkim",
-      passwd="1234",
-    )
-    if mydb.is_connected():
-      print("MySQL 데이터베이스에 성공적으로 연결되었습니다!")
-    return mydb
-  except Error as e:
-    print(f"데이터베이스 연결에 실패했습니다: {e}")
-    return None
-  
-def create_database_if_not_exists(mydb):
-    try:
-        cursor = mydb.cursor()
-        cursor.execute(f"CREATE DATABASE IF NOT EXISTS software_club")
-        mydb.commit()
-        cursor.close()
-    except Error as e:
-        print(f"데이터베이스 생성 중 오류 발생: {e}")
-        
-def use_database(mydb):
-    try:
-        cursor = mydb.cursor()
-        cursor.execute("USE software_club")
-        cursor.close()
-        print("software_club 데이터베이스를 사용합니다.")
-    except Error as e:
-        print(f"데이터베이스 사용 중 오류 발생: {e}")
-
-def init_database(mydb, file_path):
-  try:
-    cursor = mydb.cursor()
-    with open(file_path, 'r') as sql_file:
-      sql_script = sql_file.read()
-      commands = sql_script.split(';')  # SQL 명령어를 ';'로 분리
-      for command in commands:
-        if command.strip():  # 빈 명령어 무시
-          cursor.execute(command.strip())
-      mydb.commit()
-    cursor.close()
-    print(f"{file_path} 파일이 성공적으로 실행되었습니다.")
-  except Error as e:
-    print(f"SQL 파일 실행 중 오류 발생: {e}")
-    
 def main_menu():
   print("==============================================\n")
   print("====== 소프트웨어학부 학부 동아리 관리시스템 ======\n")
