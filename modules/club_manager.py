3a6a2c31207a6930446e83c283c4c2a27fc71e8c
YeonShin <yeonnim82@gmail.com>
2024-12-03 02:35:29 +0900

    화면 지우기 코드 추가 및 수정

--- modules/club_manager.py
+++ modules/club_manager.py
@@ -30,84 +30,84 @@ def club_manager_menu(mydb):
     
     task = input("원하는 업무를 선택하세요: ").strip()
     
-    if task == "1":
-      print("\n==================================")
-      print("1. 신규 부원 등록\t\t2. 전체 부원 조회\n")
-      print("3. 개별 부원 조회\t\t4. 부원 정보 수정\n")
-      print("5. 부원 정보 삭제\n")
-      print("0. 뒤로가기\n")
-      choice = input("수행하고자하는 작업을 선택하세요: ").strip()
-      if choice == "1":
-        # 신규 부원 등록 (동아리장이 관리하는 특정 동아리의 부원에 한해)
-      elif choice == "2":
-        # 전체 부원 조회 (동아리장이 관리하는 특정 동아리의 부원에 한해)
-      elif choice == "3":
-        # 개별 부원 조회 (동아리장이 관리하는 특정 동아리의 부원에 한해)
-      elif choice == "4":
-        # 부원 정보 수정 (동아리장이 관리하는 특정 동아리의 부원에 한해, 직책은 수정 불가)
-      elif choice == "5":
-        # 부원 정보 삭제 (동아리장이 관리하는 특정 동아리의 부원에 한해, 삭제하면 부원이 속한 동아리가 NULL)
-      elif choice == "0":
-        # 뒤로가기
-        continue
-      else:
-        # 잘못된 접근
-    elif task == "2":
-      print("\n==================================")
-      print("1. 신규 활동 등록\t\t2. 활동 정보 조회\n")
-      print("3. 활동 정보 수정\t\t4. 활동 정보 삭제\n")
+    # if task == "1":
+    #   print("\n==================================")
+    #   print("1. 신규 부원 등록\t\t2. 전체 부원 조회\n")
+    #   print("3. 개별 부원 조회\t\t4. 부원 정보 수정\n")
+    #   print("5. 부원 정보 삭제\n")
+    #   print("0. 뒤로가기\n")
+    #   choice = input("수행하고자하는 작업을 선택하세요: ").strip()
+    #   if choice == "1":
+    #     # 신규 부원 등록 (동아리장이 관리하는 특정 동아리의 부원에 한해)
+    #   elif choice == "2":
+    #     # 전체 부원 조회 (동아리장이 관리하는 특정 동아리의 부원에 한해)
+    #   elif choice == "3":
+    #     # 개별 부원 조회 (동아리장이 관리하는 특정 동아리의 부원에 한해)
+    #   elif choice == "4":
+    #     # 부원 정보 수정 (동아리장이 관리하는 특정 동아리의 부원에 한해, 직책은 수정 불가)
+    #   elif choice == "5":
+    #     # 부원 정보 삭제 (동아리장이 관리하는 특정 동아리의 부원에 한해, 삭제하면 부원이 속한 동아리가 NULL)
+    #   elif choice == "0":
+    #     # 뒤로가기
+    #     continue
+    #   else:
+    #     # 잘못된 접근
+    # elif task == "2":
+    #   print("\n==================================")
+    #   print("1. 신규 활동 등록\t\t2. 활동 정보 조회\n")
+    #   print("3. 활동 정보 수정\t\t4. 활동 정보 삭제\n")
 
-      print("0. 뒤로가기\n")
-      choice = input("수행하고자하는 작업을 선택하세요: ").strip()
-      if choice == "1":
-        # 신규 활동 등록
-      elif choice == "2":
-        # 전체 활동 조회
-      elif choice == "3":
-        # 개별 활동 조회
-      elif choice == "4":
-        # 활동 내용 수정
-      elif choice == "0":
-        # 뒤로가기
-        continue
-      else:
-        # 잘못된 접근
-    elif task == "3":
-      print("\n==================================")
-      print("1. 예산내역 등록\t\t2. 예산내역 조회\n")
-      print("3. 예산내역 수정\t\t4. 예산내역 삭제\n")
-      print("0. 뒤로가기\n")
-      choice = input("수행하고자하는 작업을 선택하세요: ").strip()
-      if choice == "1":
-        # 신규 예산내역 등록
-      elif choice == "2":
-        # 예산내역 조회
-      elif choice == "3":
-        # 예산내역 수정
-      elif choice == "4":
-        # 예산내역 삭제
-      elif choice == "0":
-        # 뒤로가기
-        continue
-      else:
-        # 잘못된 접근
-    elif task == "4":
-      print("\n==================================")
-      print("1. 공지사항 목록 조회\t\t2. 공지사항 상세 조회\n")
-      print("0. 뒤로가기\n")
-      choice = input("수행하고자하는 작업을 선택하세요: ").strip()
-      if choice == "1":
-        # 공지사항 목록 조회
-      elif choice == "2":
-        # 공지사항 상세 조회
-      elif choice == "0":
-        # 뒤로가기
-        continue
-      else:
-        # 잘못된 접근
-    elif task == "0":
-      print("관리자 모드에서 로그아웃합니다.")
-      break
-    else:
-      print("잘못된 입력입니다. 다시 선택하세요.")
+    #   print("0. 뒤로가기\n")
+    #   choice = input("수행하고자하는 작업을 선택하세요: ").strip()
+    #   if choice == "1":
+    #     # 신규 활동 등록
+    #   elif choice == "2":
+    #     # 전체 활동 조회
+    #   elif choice == "3":
+    #     # 개별 활동 조회
+    #   elif choice == "4":
+    #     # 활동 내용 수정
+    #   elif choice == "0":
+    #     # 뒤로가기
+    #     continue
+    #   else:
+    #     # 잘못된 접근
+    # elif task == "3":
+    #   print("\n==================================")
+    #   print("1. 예산내역 등록\t\t2. 예산내역 조회\n")
+    #   print("3. 예산내역 수정\t\t4. 예산내역 삭제\n")
+    #   print("0. 뒤로가기\n")
+    #   choice = input("수행하고자하는 작업을 선택하세요: ").strip()
+    #   if choice == "1":
+    #     # 신규 예산내역 등록
+    #   elif choice == "2":
+    #     # 예산내역 조회
+    #   elif choice == "3":
+    #     # 예산내역 수정
+    #   elif choice == "4":
+    #     # 예산내역 삭제
+    #   elif choice == "0":
+    #     # 뒤로가기
+    #     continue
+    #   else:
+    #     # 잘못된 접근
+    # elif task == "4":
+    #   print("\n==================================")
+    #   print("1. 공지사항 목록 조회\t\t2. 공지사항 상세 조회\n")
+    #   print("0. 뒤로가기\n")
+    #   choice = input("수행하고자하는 작업을 선택하세요: ").strip()
+    #   if choice == "1":
+    #     # 공지사항 목록 조회
+    #   elif choice == "2":
+    #     # 공지사항 상세 조회
+    #   elif choice == "0":
+    #     # 뒤로가기
+    #     continue
+    #   else:
+    #     # 잘못된 접근
+    # elif task == "0":
+    #   print("관리자 모드에서 로그아웃합니다.")
+    #   break
+    # else:
+    #   print("잘못된 입력입니다. 다시 선택하세요.")
         
--- modules/department_manager.py
+++ modules/department_manager.py
@@ -1,3 +1,7 @@
+from modules.club_management import *
+from modules.member_management import *
+import os
+
 # 관리자 로그인
 def login_department_manager(mydb):
   employee_id = input("직원번호 8자리를 입력해주세요: ").strip()
@@ -8,94 +12,110 @@ def login_department_manager(mydb):
   cursor.close()
   
   if department_manager:
+    os.system("clear")
     print(f"환영합니다, {department_manager['Ename']}님!")
+    return True
   else:
+    os.system("clear")
     print("올바르지 않은 직원번호입니다.")
+    return False
     
 def department_manager_menu(mydb):
   """
   학부 관리자 메뉴
   """
   while True:
-    print("\n========== 학부 관리자 모드 ==========")
-    print("1. 동아리 관리\n")
-    print("2. 부원 관리\n")
-    print("3. 공지사항 관리\n")
-    print("0. 종료")
+    print("\n======= 학부 관리자 모드 =======")
+    print("1. 동아리 관리")
+    print("2. 부원 관리")
+    print("3. 공지사항 관리")
+    print("0. 종료\n")
     
     task = input("원하는 업무를 선택하세요: ").strip()
     
     if task == "1":
-      print("\n==================================")
-      print("1. 동아리 개설\t\t2. 전체 동아리 조회\n")
-      print("3. 기본 정보 수정\t\t4. 동아리 삭제\n")
-      print("5. 수상 실적 등록\t\t6. 수상 실적 조회\n")
-      print("7. 수상 실적 수정\t\t8. 수상 실적 삭제\n")
-      print("9. 활동 정보 조회\t\t10. 예산 내역 조회\n")
+      os.system("clear")
+      print("============== 동아리 관리 =================")
+      print("1. 동아리 개설\t\t\t2. 전체 동아리 조회")
+      print("3. 기본 정보 수정\t\t4. 동아리 삭제")
+      print("5. 수상 실적 등록\t\t6. 수상 실적 조회")
+      print("7. 수상 실적 수정\t\t8. 수상 실적 삭제")
+      print("9. 활동 정보 조회\t\t10. 예산 내역 조회")
       print("0. 뒤로가기\n")
       choice = input("수행하고자하는 작업을 선택하세요: ").strip()
       if choice == "1":
         # 동아리 개설
+        create_club(mydb)
       elif choice == "2":
-        # 전체 동아리 조회
+        select_all_clubs(mydb)
       elif choice == "3":
-        # 기본 정보 수정
+        update_club_info(mydb)
       elif choice == "4":
-        # 동아리 삭제
-      elif choice == "5":
+        delete_club(mydb)
+      # elif choice == "5":
         # 수상 실적 등록
-      elif choice == "6":
-        # 수상 실적 조회
-      elif choice == "7":
-        # 수상 실적 수정
-      elif choice == "8":
-        # 수상 실적 삭제
-      elif choice == "9":
-        # 활동 정보 조회
-      elif choice == "10":
-        # 예산 내역 조회
+      # elif choice == "6":
+      #   # 수상 실적 조회
+      # elif choice == "7":
+      #   # 수상 실적 수정
+      # elif choice == "8":
+      #   # 수상 실적 삭제
+      # elif choice == "9":
+      #   # 활동 정보 조회
+      # elif choice == "10":
+      #   # 예산 내역 조회
       elif choice == "0":
         # 뒤로가기
+        os.system("clear")
+        print("이전으로 돌아갑니다.")
         continue
       else:
         # 잘못된 접근
+        os.system("clear")
+        print("잘못된 접근")
     elif task == "2":
-      print("\n==================================")
+      os.system("clear")
+      print("============== 부원 관리 =================")
       print("1. 전체 부원 조회\t\t2. 개별 부원 조회\n")
       print("3. 부원 직책 변경\n")
       print("0. 뒤로가기\n")
       choice = input("수행하고자하는 작업을 선택하세요: ").strip()
       if choice == "1":
-        # 전체 부원 조회
+        view_all_members(mydb)
       elif choice == "2":
-        # 개별 부원 조회
+        search_member_by_name(mydb)
       elif choice == "3":
-        # 부원 직책 변경
+        assign_club_manager(mydb)
       elif choice == "0":
         # 뒤로가기
+        os.system("clear")
+        print("이전으로 돌아갑니다.")
         continue
       else:
         # 잘못된 접근
-    elif task == "3":
-      print("\n==================================")
-      print("1. 공지사항 게시\t\t2. 공지사항 조회\n")
-      print("3. 공지사항 수정\t\t4. 공지사항 삭제\n")
-      print("0. 뒤로가기\n")
-      choice = input("수행하고자하는 작업을 선택하세요: ").strip()
-      if choice == "1":
-        # 공지사항 게시
-      elif choice == "2":
-        # 공지사항 조회
-      elif choice == "3":
-        # 공지사항 수정
-      elif choice == "4":
-        # 공지사항 삭제
-      elif choice == "0":
-        # 뒤로가기
-        continue
-      else:
-        # 잘못된 접근
+        os.system("clear")
+        print("잘못된 접근")
+    # elif task == "3":
+    #   print("\n==================================")
+    #   print("1. 공지사항 게시\t\t2. 공지사항 조회\n")
+    #   print("3. 공지사항 수정\t\t4. 공지사항 삭제\n")
+    #   print("0. 뒤로가기\n")
+    #   choice = input("수행하고자하는 작업을 선택하세요: ").strip()
+    #   if choice == "1":
+    #     # 공지사항 게시
+    #   elif choice == "2":
+    #     # 공지사항 조회
+    #   elif choice == "3":
+    #     # 공지사항 수정
+    #   elif choice == "4":
+    #     # 공지사항 삭제
+    #   elif choice == "0":
+    #     # 뒤로가기
+    #     continue
+    #   else:
+    #     # 잘못된 접근
     elif task == "0":
+      os.system("clear")
       print("관리자 모드에서 로그아웃합니다.")
       break
     else:
